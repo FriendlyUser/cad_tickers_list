f08a06e413ba17ea4faee1e33edb21a97f5ca14c
dli <davidli012345@gmail.com>
2020-08-01 15:11:51 -0700

    adding cron job tmmr for test

---
+++ content/en/background.md
@@ -0,0 +1,13 @@
+---
+title: Background Info
+description: 'Background information on why this project exists'
+position: 2
+category: Getting started
+version: 1.4
+fullscreen: false
+---
+
+
+After creating a personal project for scrapping canadian tickers I meant to complete ages ago, [https://github.com/FriendlyUser/cad_tickers](cad_tickers).
+
+As an investor I intend to go through the list of stocks every month, this script helps me archieve and do that/
--- scripts/make_md_file.py
+++ scripts/make_md_file.py
@@ -8,14 +8,16 @@ currentYear = datetime.now().year
 folder = f'static/{currentMonth}'
 tsx_csv = f"{folder}/tsx_{currentMonth}_{currentYear}.csv"
 tsx_df = pd.read_csv(tsx_csv)
+# drop undesired rows
+tsx_df = tsx_df.drop(['Unnamed: 0', 'QMV($)','HQ Region'],axis=1, errors='ignore')
 tsx_json = tsx_df.to_json(orient='split')
-tsx_html = tsx_df.to_html()
+tsx_html = tsx_df.to_html(index=False,index_names=False, show_dimensions=True)
 
 cse_csv = f"{folder}/cse_{currentMonth}_{currentYear}.csv"
-cse_df = pd.read_csv(tsx_csv)
+cse_df = pd.read_csv(cse_csv)
+cse_df = cse_df.drop(['Unnamed: 0', 'Currency','urls'],axis=1, errors='ignore')
 cse_json = cse_df.to_json(orient='split')
-cse_html = cse_df.to_html()
-
+cse_html = cse_df.to_html(index=False,index_names=False, show_dimensions=True)
 
 meta = dict(
   title = f"Tickers for {currentMonth}/{currentYear}",
