42540412149ce34e97620a3d65fc69c98c4e12b4
dli <davidli012345@gmail.com>
2020-10-31 08:49:18 -0700

    updating stocks generation

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -1,19 +1,11 @@
-from cad_tickers.exchanges.tsx import get_all_tickers_data
+from cad_tickers.exchanges.all_tickers import get_all_tickers_data
 from datetime import datetime
 import pathlib
 
 currentMonth = datetime.now().month
-folder = f"static/{currentMonth}"
+folder = f"static/latest"
 pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
 
 df = get_all_tickers_data()
-currentYear = datetime.now().year
-tsx_name = f"{folder}/tsx_{currentMonth}_{currentYear}.csv"
-df.to_csv(tsx_name, index=False)
-
-from cad_tickers.exchanges.cse import get_cse_tickers_df, add_descriptions_to_df
-
-cse_df = get_cse_tickers_df()
-cse_df = add_descriptions_to_df(cse_df)
-cse_name = f"{folder}/cse_{currentMonth}_{currentYear}.csv"
-cse_df.to_csv(cse_name, index=False)
+full_name = f"{folder}/stocks.csv"
+df.to_csv(full_name, index=False)
