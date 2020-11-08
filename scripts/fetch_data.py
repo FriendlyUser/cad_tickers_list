18327f33e4b7f6990272e6e511a47fa1d2384227
dli <davidli012345@gmail.com>
2020-11-07 19:26:51 -0800

    updating mk full tickers

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -1,4 +1,4 @@
-from cad_tickers.exchanges.all_tickers import get_all_tickers_data
+from cad_tickers.exchanges.all_tickers import mk_full_tickers
 from datetime import datetime
 import pathlib
 
@@ -6,6 +6,6 @@ currentMonth = datetime.now().month
 folder = f"static/latest"
 pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
 
-df = get_all_tickers_data()
+df = mk_full_tickers()
 full_name = f"{folder}/stocks.csv"
 df.to_csv(full_name, index=False)
