14b0a85ab9bd2e4aae20ab9d2b0cbcacba17d348
dli <davidli012345@gmail.com>
2020-11-07 19:19:41 -0800

    getting the build working again

--- scripts/make_md_file.py
+++ scripts/make_md_file.py
@@ -6,32 +6,36 @@ from jinja2 import Template
 currentMonth = datetime.now().month
 currentYear = datetime.now().year 
 folder = f'static/{currentMonth}'
-tsx_csv = f"{folder}/tsx_{currentMonth}_{currentYear}.csv"
-tsx_df = pd.read_csv(tsx_csv)
-# drop undesired rows
-tsx_df = tsx_df.drop(['Unnamed: 0', 'QMV($)','HQ Region'],axis=1, errors='ignore')
-tsx_json = tsx_df.to_json(orient='split')
-tsx_html = tsx_df.to_html(index=False,index_names=False, show_dimensions=True)
 
-cse_csv = f"{folder}/cse_{currentMonth}_{currentYear}.csv"
-cse_df = pd.read_csv(cse_csv)
-cse_df = cse_df.drop(['Unnamed: 0', 'Currency','urls'],axis=1, errors='ignore')
-cse_json = cse_df.to_json(orient='split')
-cse_html = cse_df.to_html(index=False,index_names=False, show_dimensions=True)
+# TODO fix later
+# Streamlit site is a much better idea
 
-meta = dict(
-  title = f"Tickers for {currentMonth}/{currentYear}",
-  position = currentMonth,
-  description = f"Tickers for {currentMonth}/{currentYear}",
-  version = '1.0',
-  category = 'Tickers'
-)
+# tsx_csv = f"{folder}/tsx_{currentMonth}_{currentYear}.csv"
+# tsx_df = pd.read_csv(tsx_csv)
+# # drop undesired rows
+# tsx_df = tsx_df.drop(['Unnamed: 0', 'QMV($)','HQ Region'],axis=1, errors='ignore')
+# tsx_json = tsx_df.to_json(orient='split')
+# tsx_html = tsx_df.to_html(index=False,index_names=False, show_dimensions=True)
 
-options=dict(meta=meta, cse_tickers=cse_html, tsx_tickers=tsx_html)
-with open("scripts/tickers.jinja2") as file_:
-  template = Template(file_.read())
-tickers = template.render(**options)
+# cse_csv = f"{folder}/cse_{currentMonth}_{currentYear}.csv"
+# cse_df = pd.read_csv(cse_csv)
+# cse_df = cse_df.drop(['Unnamed: 0', 'Currency','urls'],axis=1, errors='ignore')
+# cse_json = cse_df.to_json(orient='split')
+# cse_html = cse_df.to_html(index=False,index_names=False, show_dimensions=True)
 
-md_file = f'content/en/{currentMonth}_{currentYear}.md'
-with open(md_file, 'w', errors='ignore') as file_:
-  file_.write(tickers)
+# meta = dict(
+#   title = f"Tickers for {currentMonth}/{currentYear}",
+#   position = currentMonth,
+#   description = f"Tickers for {currentMonth}/{currentYear}",
+#   version = '1.0',
+#   category = 'Tickers'
+# )
+
+# options=dict(meta=meta, cse_tickers=cse_html, tsx_tickers=tsx_html)
+# with open("scripts/tickers.jinja2") as file_:
+#   template = Template(file_.read())
+# tickers = template.render(**options)
+
+# md_file = f'content/en/{currentMonth}_{currentYear}.md'
+# with open(md_file, 'w', errors='ignore') as file_:
+#   file_.write(tickers)
