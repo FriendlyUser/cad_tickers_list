d93e9f7bd883f462f7fac6726cba97dba0c5cc56
David Li <davidli012345@gmail.com>
2021-08-19 14:53:57 +0000

    saving cse.csv

--- scripts/get_ticker_list.py
+++ scripts/get_ticker_list.py
@@ -3,6 +3,7 @@ import requests
 import os
 import pathlib
 import json
+from cad_tickers.exchanges.cse import get_cse_tickers_df
 
 folder = f"static/latest"
 pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
@@ -15,7 +16,6 @@ def get_tickers():
     data = resp.get("data")
     return data
 
-
 file_name = f"{folder}/tickers"
 raw_file_name = f"{folder}/raw_tickers.json"
 full_tickers = get_tickers()
@@ -25,3 +25,7 @@ with open(file_name, "wb") as fp:
 with open(raw_file_name, 'w', encoding='utf-8') as f:
     json.dump(full_tickers, f, ensure_ascii=False, indent=4)
     
+
+cse_df = get_cse_tickers_df()
+cse_path = f"{folder}/cse.csv"
+cse_df.to_csv(cse_path,index=False)
